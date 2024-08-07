e4ddb1437428c8fa428cc9baaebc802e3c02ac23
Julio <julioacostaqhlo@gmail.com>
2024-08-08 04:16:32 +0800

    cors update

--- who-is-back-end/config/cors.php
+++ who-is-back-end/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:5000'],
 
     'allowed_origins_patterns' => [],
 
